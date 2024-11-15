162c3cfd523276b75fc37196cf8355e6b91d09a0
martetosca9 <martoscani509@gmail.com>
2024-11-14 23:42:18 -0300

    dashboard

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,14 @@
+
+import { requireUser } from "../lib/hooks";
+
+
+export default async function DashboardPage() {
+
+    const session = await requireUser();
+
+    return (
+        <div>
+            <h1>Dashboard123</h1>
+        </div>
+    )
+}
